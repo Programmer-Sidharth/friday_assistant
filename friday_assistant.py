c8e57c15a1cb8522c3d76d12b4038561be031a96
Ram Bhardwaj <63110845+Programmer-Sidharth@users.noreply.github.com>
2020-05-30 18:35:33 +0530

    added functionality to do tasks with chat

--- friday_assistant.py
+++ friday_assistant.py
@@ -6,13 +6,29 @@ r = requests.get(url)
 
 data = r.json()
 
+def do_task(task, line):
+	q = line
+	for work in task:
+		if work == 'calculate':
+			abc = 'abcdefghijklmnopqrstuvwxyz'
+			for word in abc:
+				q = q.replace(word, '')
+			ans = eval(q)
+			print(ans)
+		if work == 'coronavirus':
+			# use corona api
+			print('coronavirus cases are on Ram\'s website')
+
 def response(line):
 	result = []
+	tasks = []
 	for values in data:
 		valuesList = values['hotwords'].split('|')
 		for hotword in valuesList:
 			if hotword in line:
 				result.append(random.choice(values['responses'].split('|')))
+				if instance['tasks'] != None or '':
+					tasks.append(values['tasks'])
 	return result
 
 while 1:
